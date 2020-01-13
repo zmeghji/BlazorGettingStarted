2c75f013960da9aa27b1530f397e2964301e4014
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2020-01-12 19:56:52 -0500

    completed module 2

---
+++ BlazorGettingStarted/Pages/TaskDetailBase.cs
@@ -0,0 +1,21 @@
+﻿using BlazorGettingStarted.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorGettingStarted.Pages
+{
+    public class TaskDetailBase : ComponentBase
+    {
+        [Parameter]
+        public string TaskId { get; set; }
+        public TaskItem TaskItem { get; set; }
+        protected override Task OnInitializedAsync()
+        {
+            TaskItem = TaskItem.CreateTaskItems().Single(t => t.Id == Convert.ToInt32(TaskId));
+            return base.OnInitializedAsync(); 
+        }
+    }
+}
---
+++ BlazorGettingStarted/Pages/TaskOverviewBase.cs
@@ -0,0 +1,22 @@
+﻿using BlazorGettingStarted.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorGettingStarted.Pages
+{
+    public class TaskOverviewBase :ComponentBase
+    {
+        public List<TaskItem> TaskItems { get; set; } = new List<TaskItem>();
+        protected override Task OnInitializedAsync()
+        {
+            for(var i =0; i<5; i++)
+            {
+                TaskItems.Add(new TaskItem { Id = i, Name = $"{i}{i}{i}{i}{i}" });
+            }
+            return base.OnInitializedAsync();
+        }
+    }
+}
